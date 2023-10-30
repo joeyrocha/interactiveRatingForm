0363cc9c5a18c8b6174d462b75669b94cdf0d2d8
Joey Rocha <jrochaperez123@gmail.com>
2023-10-29 21:23:23 -0700

    2nd push

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-const ratingForm = document.getElementById("ratingForm")
-const thankYouForm= documnet.getElementById("thankYouForm")
-const numberBtn = document.getElementsByClassName(".number_btn")
-const submitBtn = document.getElementById("submitButton")
+const ratingForm = document.getElementById("ratingForm");
+const thankYouForm = document.getElementById("thankYouForm");
+const submitButton = document.getElementById("submitButton");
+const selectedRatingElement = document.getElementById("selectedRating"); // Added an element to display the selected rating
 
-let feedback = 5
+const ratingButtons = document.querySelectorAll(".number_btn");
+let selectedRating = null; // Variable to store the selected rating
 
-numberBtn.onclick = function() {
-    window.location.href = "https://www.google.com";
-    };
-//when the button of class number_btn is clicked i would like it to be highlighted as well as well and keep the value assosiated with with the submit button is selected then transition into another form for the thank you page 
+ratingButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    selectedRating = button.getAttribute("data-value"); // Store the selected rating
+    selectedRatingElement.textContent = `You selected: ${selectedRating}`; // Display the selected rating
+  });
+});
 
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (selectedRating !== null) {
+    // Handle form submission here (e.g., show the thank you form)
+    ratingForm.style.display = "none";
+    thankYouForm.style.display = "block";
+  } else {
+    alert("Please select a rating before submitting.");
+  }
+});
+/*
+Code Structure:
 
-// When the ratilng is selected how would i make the button have the value assosiated with it
+- Variables:
+  - ratingForm: Represents the form where users select a rating.
+  - thankYouForm: Represents the form that appears after rating submission.
+  - submitButton: Represents the button for submitting the rating.
+  - selectedRatingElement: Represents the element for displaying the selected rating.
+  - ratingButtons: NodeList containing rating buttons.
+  - selectedRating: Variable to store the selected rating.
 
-if(feedback >= 1 && feedback <= 5) {
-    
-}
+- Event Listeners:
+  - For Each Rating Button (button in ratingButtons):
+    - When a button is clicked, it executes the following:
+      - Set selectedRating to the rating value associated with the clicked button.
+      - Update selectedRatingElement to display the selected rating value.
+
+  - For the Submit Button (submitButton):
+    - When the submit button is clicked, it executes the following:
+      - Prevent the default form submission behavior.
+      - Check if a rating has been selected (selectedRating is not null).
+      - If a rating is selected:
+        - Handle the form submission (e.g., show the thankYouForm and hide the ratingForm).
+      - If no rating is selected:
+        - Display an alert to remind the user to select a rating before submitting.
+*/
